d2a724e80b661414a61bd44c5d41a89d955406ea
Ivan Tejano <tejanogenard@gmail.com>
2024-07-30 09:18:35 -0700

    Initial Commit

---
+++ bouquetBoys/bouquetBoysApp.swift
@@ -0,0 +1,17 @@
+//
+//  bouquetBoysApp.swift
+//  bouquetBoys
+//
+//  Created by Genard Tejano on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct bouquetBoysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
